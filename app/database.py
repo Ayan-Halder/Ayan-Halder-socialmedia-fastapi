fc0bdd3e7df465097de0f9e51c45b2e025211b39
ayanhalder194 <ayanhalder194@gmail.com>
2022-07-03 20:15:46 +0530

    edited config.py

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ from .config import settings
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@ec2-44-195-162-77.compute-1.amazonaws.com:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
