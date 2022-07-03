241fc5b02cede606496c9b070d4a7b6b4751c807
ayanhalder194 <ayanhalder194@gmail.com>
2022-07-03 19:30:44 +0530

    edited config.py

--- app/config.py
+++ app/config.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     database_hostname: str
     database_port: str
     database_name: str
-    database_username: str = "postgres"
-    database_password: str = "localhost"
+    database_username: str
+    database_password: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
